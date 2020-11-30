7b1ee0b822a5b509ace461581b78e7d90f630874
karthiksharma <karthik.sharma@aeratechnology.com>
2020-12-01 04:03:45 +0530

    Setup driver object. Currently supporting two browsers, i.e. Chrome and Edge

--- test-framework/src/main/java/selenium_framework/Browsers.java
+++ test-framework/src/main/java/selenium_framework/Browsers.java
@@ -1,5 +1,5 @@
 package selenium_framework;
 
 public enum Browsers {
-    CHROME, FIREFOX, IE, SAFARI, EDGE
+    CHROME, FIREFOX
 }
--- test-framework/src/main/java/selenium_framework/SeleniumBaseFramework.java
+++ test-framework/src/main/java/selenium_framework/SeleniumBaseFramework.java
@@ -1,10 +1,24 @@
 package selenium_framework;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.util.Properties;
 
 public class SeleniumBaseFramework {
     public WebDriver driver;
     public Properties properties = new Properties();
+
+    private void setProperties() throws Exception {
+        properties.load(this.getClass().getResourceAsStream("/configuration.properties"));
+    }
+
+    @Parameters({"browser"})
+    @BeforeClass
+    public void browserSetup(@Optional("CHROME") String browser) throws Exception {
+        setProperties();
+        driver = WebDriverFactory.getDriver(Browsers.valueOf(browser), properties);
+    }
 }

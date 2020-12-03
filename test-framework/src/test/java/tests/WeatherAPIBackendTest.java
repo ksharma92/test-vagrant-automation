bdfd2e54a65750a8b97f9f0aaaa86e95f4b96e54
karthiksharma <karthik.sharma@aeratechnology.com>
2020-12-03 09:50:11 +0530

    Created Testng file and enabled parallel execution Made the framework CI ready

---
+++ test-framework/src/test/java/tests/WeatherAPIBackendTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class WeatherAPIBackendTest {
+
+    @Test
+    public void doSomething() {
+        System.out.println("test");
+    }
+}
--- test-framework/src/test/java/tests/WeatherTest.java
+++ test-framework/src/test/java/tests/WeatherTest.java
@@ -66,4 +66,5 @@ public class WeatherTest extends BaseTest {
 
         softAssert.assertAll();
     }
+
 }
